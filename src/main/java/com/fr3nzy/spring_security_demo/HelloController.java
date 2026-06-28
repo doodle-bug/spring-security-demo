423213a60eea2395d79a0f494f7ee61ad82efe52
Amit Anand <amit00007anand@gmail.com>
2026-06-28 15:13:58 +0530

    Initial commit (Spring security basics)

---
+++ src/main/java/com/fr3nzy/spring_security_demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.fr3nzy.spring_security_demo;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String greet(HttpServletRequest req){
+        return "Hello World! " + req.getSession().getId() ;
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest req){
+        return "Engineer " + req.getSession().getId();
+    }
+}
---
+++ src/main/java/com/fr3nzy/spring_security_demo/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.fr3nzy.spring_security_demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+
+        http
+                //        disabling csrf
+                .csrf(customizer -> customizer.disable())
+                //        we're enabling the security for all the request
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                //        making the session stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+/*
+        The above lambda expression is doing all this behind the scenes (this is just for csrf disable)
+        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+                configurer.disable();
+            }
+        };
+
+        http.csrf(custCsrf);
+*/
+/*
+        we don't need form logion when using stateless as session ID keeps changing on every request
+        http.formLogin(Customizer.withDefaults());
+*/
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("fr3nzy")
+                .password("a@1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("ad@1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
